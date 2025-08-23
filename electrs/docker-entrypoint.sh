9c36957464954e18ee0229854a840e6cbf38b61b
Katoga <katoga.cz@hotmail.com>
2025-08-23 14:57:28 +0200

    electrs: Connect to hostnames

--- electrs/docker-entrypoint.sh
+++ electrs/docker-entrypoint.sh
@@ -9,8 +9,8 @@ echo "auth = \"${BLESK_BITCOINCORE_RPC_USER}:${BLESK_BITCOINCORE_RPC_PASSWORD}\"
 
 indexer_options=(
   # Bitcoin Core settings
-  --daemon-rpc-addr="[${BLESK_BITCOINCORE_IPV6}]:${BLESK_BITCOINCORE_PORT_RPC}"
-  --daemon-p2p-addr="[${BLESK_BITCOINCORE_IPV6}]:${BLESK_BITCOINCORE_PORT_P2P}"
+  --daemon-rpc-addr="bitcoincore:${BLESK_BITCOINCORE_PORT_RPC}"
+  --daemon-p2p-addr="bitcoincore:${BLESK_BITCOINCORE_PORT_P2P}"
 
   # Electrs settings
   --electrum-rpc-addr="${BLESK_ELECTRS_IPV6}:${BLESK_ELECTRS_PORT}"
