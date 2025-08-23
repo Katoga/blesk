24a70339e319382afeaa2ab2f90e4255169854b5
Katoga <katoga.cz@hotmail.com>
2025-08-23 14:58:33 +0200

    explorer: Connect to hostnames

--- explorer/docker-entrypoint.sh
+++ explorer/docker-entrypoint.sh
@@ -10,13 +10,13 @@ explorer_options=(
   --host="$BLESK_EXPLORER_IPV6"
   --port="$BLESK_EXPLORER_PORT"
 
-  --bitcoind-host="$BLESK_BITCOINCORE_IPV6"
+  --bitcoind-host="bitcoincore"
   --bitcoind-port="$BLESK_BITCOINCORE_PORT_RPC"
   --bitcoind-user="$BLESK_BITCOINCORE_RPC_USER"
   --bitcoind-pass="$BLESK_BITCOINCORE_RPC_PASSWORD"
 
   --address-api="electrum"
-  --electrum-servers="tcp://[${BLESK_ELECTRS_IPV6}]:${BLESK_ELECTRS_PORT}"
+  --electrum-servers="tcp://electrs:${BLESK_ELECTRS_PORT}"
 
   --slow-device-mode="${BLESK_EXPLORER_SLOW_DEVICE_MODE}"
 )
