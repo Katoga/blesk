9d51482d7b31ed8aa2a04a07095f345699827f05
Katoga <katoga.cz@hotmail.com>
2025-08-24 12:06:07 +0200

    tor: Use IPv6 only

--- tor/docker-entrypoint.sh
+++ tor/docker-entrypoint.sh
@@ -6,10 +6,8 @@ tor_options=(
   --ClientPreferIPv6ORPort 1
 
   --SocksPort "[${BLESK_TOR_IPV6}]:${BLESK_TOR_PORT_SOCKS}"
-  --SocksPort "${BLESK_TOR_IPV4}:${BLESK_TOR_PORT_SOCKS}"
 
   --SocksPolicy "accept ${BLESK_NET_INTERNAL_IPV6}"
-  --SocksPolicy "accept ${BLESK_NET_INTERNAL_IPV4}"
   --SocksPolicy "reject *"
 
   --DataDirectory "${BLESK_TOR_HOME}/.tor"
