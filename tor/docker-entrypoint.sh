7c1b342dc6d4f877063f66d145372e8b3aca5431
Katoga <katoga.cz@hotmail.com>
2025-08-26 22:06:11 +0200

    tor: Use config instead of CLI args

--- tor/docker-entrypoint.sh
+++ tor/docker-entrypoint.sh
@@ -2,44 +2,6 @@
 
 set -euo pipefail
 
-tor_options=(
-  --ClientPreferIPv6ORPort 1
+envsubst < /etc/tor/torrc.template > /etc/tor/torrc
 
-  --SocksPort "[${BLESK_TOR_IPV6}]:${BLESK_TOR_PORT_SOCKS}"
-
-  --SocksPolicy "accept ${BLESK_NET_INTERNAL_IPV6}"
-  --SocksPolicy "reject *"
-
-  --DataDirectory "${BLESK_TOR_HOME}/.tor"
-  --DataDirectoryGroupReadable 1
-
-  --CookieAuthentication 1
-
-  --ControlPort "$BLESK_TOR_PORT_CONTROL"
-
-  # bitcoincore
-  --HiddenServiceDir "${BLESK_TOR_ONIONS}/${BLESK_BITCOINCORE_USERNAME}"
-  --HiddenServiceVersion 3
-  --HiddenServicePort "${BLESK_BITCOINCORE_PORT_P2P} [${BLESK_BITCOINCORE_IPV6}]:${BLESK_BITCOINCORE_PORT_P2P}"
-  --HiddenServiceDirGroupReadable 1
-
-  # electrs
-  --HiddenServiceDir "${BLESK_TOR_ONIONS}/${BLESK_ELECTRS_USERNAME}"
-  --HiddenServiceVersion 3
-  --HiddenServicePort "${BLESK_ELECTRS_PORT} [${BLESK_ELECTRS_IPV6}]:${BLESK_ELECTRS_PORT}"
-  --HiddenServiceDirGroupReadable 1
-
-  # explorer
-  --HiddenServiceDir "${BLESK_TOR_ONIONS}/${BLESK_EXPLORER_USERNAME}"
-  --HiddenServiceVersion 3
-  --HiddenServicePort "80 [${BLESK_EXPLORER_IPV6}]:${BLESK_EXPLORER_PORT}"
-  --HiddenServiceDirGroupReadable 1
-
-  # dojo
-  --HiddenServiceDir "${BLESK_TOR_ONIONS}/${BLESK_DOJO_USERNAME}"
-  --HiddenServiceVersion 3
-  --HiddenServicePort "80 [${BLESK_NGINX_IPV6}]:${BLESK_NGINX_PORT}"
-  --HiddenServiceDirGroupReadable 1
-)
-
-exec tor "${tor_options[@]}"
+exec tor
