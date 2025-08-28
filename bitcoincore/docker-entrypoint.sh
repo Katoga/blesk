bd5d735b4a0505eaea5f8df051c84d0a26233fbf
Katoga <katoga.cz@hotmail.com>
2025-08-28 07:38:52 +0200

    bitcoincore: Use config instead of CLI args

--- bitcoincore/docker-entrypoint.sh
+++ bitcoincore/docker-entrypoint.sh
@@ -8,36 +8,14 @@ export MALLOC_ARENA_MAX=1
 # Generate RPC auth payload
 BITCOIND_RPC_AUTH=$(rpcauth "$BLESK_BITCOINCORE_RPC_USER" "$BLESK_BITCOINCORE_RPC_PASSWORD" 2> /dev/null)
 
+export BITCOIND_RPC_AUTH
+envsubst < "${BLESK_BITCOINCORE_HOME}/bitcoin.conf.template" > "${BLESK_BITCOINCORE_HOME}/.bitcoin/bitcoin.conf"
+export -n BITCOIND_RPC_AUTH
+chmod 0600 "${BLESK_BITCOINCORE_HOME}/.bitcoin/bitcoin.conf"
+rm -f "${BLESK_BITCOINCORE_HOME}/bitcoin.conf.template"
+
 bitcoind_options=(
-  # Bitcoin daemon
-  -server=1
-  -txindex=1
-
-  # Network
-  -listen=1
-  -bind="$BLESK_BITCOINCORE_IPV6"
-  -port="$BLESK_BITCOINCORE_PORT_P2P"
-  -dns=0
-  -dnsseed=0
-
-  # TOR
-  -proxy="[${BLESK_TOR_IPV6}]:${BLESK_TOR_PORT_SOCKS}"
   -externalip="$(cat "${BLESK_TOR_ONIONS}/${BLESK_BITCOINCORE_USERNAME}/hostname")"
-
-  # Connections
-  -rpcallowip="$BLESK_NET_INTERNAL_IPV6"
-  -rpcbind="$BLESK_BITCOINCORE_IPV6"
-  -rpcport="$BLESK_BITCOINCORE_PORT_RPC"
-  -rpcauth="$BITCOIND_RPC_AUTH"
-  -zmqpubrawblock="tcp://bitcoincore:${BLESK_BITCOINCORE_PORT_ZMQ_BLOCK}"
-  -zmqpubrawtx="tcp://bitcoincore:${BLESK_BITCOINCORE_PORT_ZMQ_TX}"
-
-  # RPi optimizations
-  -maxconnections="$BLESK_BITCOINCORE_MAX_CONNECTIONS"
-  -maxuploadtarget="$BLESK_BITCOINCORE_MAX_UPLOAD_TARGET"
-
-  # DoS defence avoidance, e.g. for initial sync of Electrs
-  -whitelist="$BLESK_NET_INTERNAL_IPV6"
 )
 if [[ "${BLESK_BITCOINCORE_INITIAL_RUN:-0}" -eq 1 ]]; then
   # Initial block download optimizations
