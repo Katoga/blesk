97ff33b88185fa99ab68ac8bb6827bdc273392e9
Katoga <katoga.cz@hotmail.com>
2025-08-28 19:25:26 +0200

    explorer: Use config instead of CLI args

--- explorer/docker-entrypoint.sh
+++ explorer/docker-entrypoint.sh
@@ -2,23 +2,8 @@
 
 set -euo pipefail
 
-explorer_options=(
-  --privacy-mode
-  --no-rates
-  --coin="BTC"
+envsubst < /etc/btc-rpc-explorer/.env.template > /etc/btc-rpc-explorer/.env
+chmod 0600 /etc/btc-rpc-explorer/.env
+rm -f /etc/btc-rpc-explorer/.env.template
 
-  --host="$BLESK_EXPLORER_IPV6"
-  --port="$BLESK_EXPLORER_PORT"
-
-  --bitcoind-host="bitcoincore"
-  --bitcoind-port="$BLESK_BITCOINCORE_PORT_RPC"
-  --bitcoind-user="$BLESK_BITCOINCORE_RPC_USER"
-  --bitcoind-pass="$BLESK_BITCOINCORE_RPC_PASSWORD"
-
-  --address-api="electrum"
-  --electrum-servers="tcp://electrs:${BLESK_ELECTRS_PORT}"
-
-  --slow-device-mode="${BLESK_EXPLORER_SLOW_DEVICE_MODE}"
-)
-
-exec ./bin/cli.js "${explorer_options[@]}"
+exec ./bin/cli.js
