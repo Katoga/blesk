9af99513204040a01a074c739446cb5c18092122
Katoga <katoga.cz@hotmail.com>
2025-08-28 19:02:41 +0200

    electrs: Use config instead of CLI args

--- electrs/docker-entrypoint.sh
+++ electrs/docker-entrypoint.sh
@@ -2,26 +2,7 @@
 
 set -euo pipefail
 
-readonly conf_file="${BLESK_ELECTRS_HOME}/electrs.toml"
-touch "$conf_file"
-chmod 0640 "$conf_file"
-echo "auth = \"${BLESK_BITCOINCORE_RPC_USER}:${BLESK_BITCOINCORE_RPC_PASSWORD}\"" > "$conf_file"
+envsubst < /etc/electrs/config.toml.template > /etc/electrs/config.toml
+chmod 0640 /etc/electrs/config.toml
 
-indexer_options=(
-  # Bitcoin Core settings
-  --daemon-rpc-addr="bitcoincore:${BLESK_BITCOINCORE_PORT_RPC}"
-  --daemon-p2p-addr="bitcoincore:${BLESK_BITCOINCORE_PORT_P2P}"
-
-  # Electrs settings
-  --electrum-rpc-addr="${BLESK_ELECTRS_IPV6}:${BLESK_ELECTRS_PORT}"
-  --db-dir="${BLESK_ELECTRS_HOME}/db"
-
-  # Logging
-  --log-filters="INFO"
-
-  # Index
-  --index-lookup-limit="$BLESK_ELECTRS_LOOKUP_LIMIT"
-  --index-batch-size="$BLESK_ELECTRS_BATCH_SIZE"
-)
-
-exec electrs "${indexer_options[@]}"
+exec electrs
