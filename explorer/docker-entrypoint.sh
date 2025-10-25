b34ae24cae098ac48ac6bb9c3b39b9ba1594cebe
Katoga <katoga.cz@hotmail.com>
2025-10-25 07:55:59 +0200

    explorer: Fix restarting the container

--- explorer/docker-entrypoint.sh
+++ explorer/docker-entrypoint.sh
@@ -4,6 +4,5 @@ set -euo pipefail
 
 envsubst < /etc/btc-rpc-explorer/.env.template > /etc/btc-rpc-explorer/.env
 chmod 0600 /etc/btc-rpc-explorer/.env
-rm -f /etc/btc-rpc-explorer/.env.template
 
 exec ./explorer/bin/cli.js
