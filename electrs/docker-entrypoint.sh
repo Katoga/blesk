94b62ea9c4b9726728e66b7a2f2fdca90521f19d
Katoga <katoga.cz@hotmail.com>
2026-01-26 13:43:02 +0100

    electrs: Prepare for read-only FS

--- electrs/docker-entrypoint.sh
+++ electrs/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-envsubst < /etc/electrs/config.toml.template > /etc/electrs/config.toml
-chmod 0640 /etc/electrs/config.toml
+envsubst < /tmp/config.toml.template > /etc/electrs/config.toml
+chmod 0600 /etc/electrs/config.toml
 
 exec electrs
