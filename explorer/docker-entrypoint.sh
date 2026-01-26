87c916fb1815bb0229e4bdb240c117419b6cab20
Katoga <katoga.cz@hotmail.com>
2026-01-26 18:38:48 +0100

    explorer: Prepare for read-only FS

--- explorer/docker-entrypoint.sh
+++ explorer/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-envsubst < /etc/btc-rpc-explorer/.env.template > /etc/btc-rpc-explorer/.env
+envsubst < /tmp/.env.template > /etc/btc-rpc-explorer/.env
 chmod 0600 /etc/btc-rpc-explorer/.env
 
 exec ./explorer/bin/cli.js
